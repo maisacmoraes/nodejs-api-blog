1219061647e52dcc422ca4ba311ce7aaec542ef9
maisacmoraes <maisa.moraes@outlook.com>
2025-06-03 12:09:50 -0300

    fix: metodo delete/post/:id

--- src/controllers/Posts.controller.js
+++ src/controllers/Posts.controller.js
@@ -1,104 +1,113 @@
-const { postsService } = require('../services');
+const { postsService } = require("../services");
 
 const getPosts = async (_req, res) => {
-    try {
-      const posts = await postsService.getPosts();
-      res.status(200).json(posts);
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
-  };
+  try {
+    const posts = await postsService.getPosts();
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 
 const getPostById = async (req, res) => {
-    try {
+  try {
     const { id } = req.params;
 
     const post = await postsService.getPostById(id);
 
-    if (!post) return res.status(404).json({ message: 'Post does not exist' });
+    if (!post) return res.status(404).json({ message: "Post does not exist" });
 
     return res.status(200).json(post);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 const updatePost = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { title, content } = req.body;
-        const { data } = req.payload;
-    
-        if (!title || !content) { 
-            return res.status(400).json({ message: 'Some required fields are missing' });
-        }
-    
-        const post = await postsService.getPostById(id);
-        if (data.id !== post.id) {
-            return res.status(401).json({ message: 'Unauthorized user' });
-        }
-    
-        await postsService.updatePost({ id, title, content });
-    
-        const updatedPost = await postsService.getPostById(id);
-    
-        return res.status(200).json(updatedPost);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
+  try {
+    const { id } = req.params;
+    const { title, content } = req.body;
+    const { data } = req.payload;
+
+    if (!title || !content) {
+      return res
+        .status(400)
+        .json({ message: "Some required fields are missing" });
     }
+
+    const post = await postsService.getPostById(id);
+    if (data.id !== post.userId) {
+      return res.status(401).json({ message: "Unauthorized user" });
+    }
+
+    await postsService.updatePost({ id, title, content });
+
+    const updatedPost = await postsService.getPostById(id);
+
+    return res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 const deletePost = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { data } = req.payload;
-    
-        const post = await postsService.getPostById(id);
+  try {
+    const { id } = req.params;
+    const { data } = req.payload;
 
-        if (data.id !== post.id) {
-            return res.status(401).json({ message: 'Unauthorized user' });
-        }
-    
-        if (!post) {
-            return res.status(404).json({ message: 'Post does not exist' });
-        }
-    
-        const deletedPost = await postsService.deletePost(id);
-        return res.status(204).json(deletedPost);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
+    const post = await postsService.getPostById(id);
+
+    if (!post) {
+      return res.status(404).json({ message: "Post does not exist" });
     }
+
+    if (data.id !== post.userId) {
+      return res.status(401).json({ message: "Unauthorized user" });
+    }
+
+    const deletedPost = await postsService.deletePost(id);
+    return res.status(204).json(deletedPost);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 const searchPost = async (req, res) => {
-    try {
-        const { q } = req.query;
-      
-        const post = await postsService.searchPost(q);
-      
-        return res.status(200).json(post);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
-  };
+  try {
+    const { q } = req.query;
 
-  const createPost = async (req, res) => {
-    const { title, content, categoryIds } = req.body;
-    const { data } = req.payload;
+    const post = await postsService.searchPost(q);
 
-    console.log(data.id);
+    return res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 
-    if (!title || !content || !categoryIds) {
-        return res.status(400).json({ message: 'Some required fields are missing' });
-    }
-    
-    const validateId = (await postsService.validateCategoryIds(categoryIds)).some((id) => !id);
+const createPost = async (req, res) => {
+  const { title, content, categoryIds } = req.body;
+  const { data } = req.payload;
 
-    if (validateId) {
-        return res.status(400).json({ message: 'one or more "categoryIds" not found' }); 
-    }
-   
-  const post = await postsService.createPost({ title,
+  console.log(data.id);
+
+  if (!title || !content || !categoryIds) {
+    return res
+      .status(400)
+      .json({ message: "Some required fields are missing" });
+  }
+
+  const validateId = (await postsService.validateCategoryIds(categoryIds)).some(
+    (id) => !id
+  );
+
+  if (validateId) {
+    return res
+      .status(400)
+      .json({ message: 'one or more "categoryIds" not found' });
+  }
+
+  const post = await postsService.createPost({
+    title,
     content,
     userId: data.id,
     categoryIds,
@@ -107,6 +116,13 @@ const searchPost = async (req, res) => {
   });
 
   return res.status(201).json(post);
-  };
-  
-module.exports = { getPosts, getPostById, updatePost, searchPost, deletePost, createPost };
+};
+
+module.exports = {
+  getPosts,
+  getPostById,
+  updatePost,
+  searchPost,
+  deletePost,
+  createPost,
+};
